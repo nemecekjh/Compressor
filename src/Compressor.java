addfbf1f1e17be13c021ad891452d3b19dc2e87c
Michal Nemecek <nemecek.jh@seznam.cz>
2021-03-22 16:41:26 +0100

    Update 2021/03/22

--- src/Compressor.java
+++ src/Compressor.java
@@ -2,36 +2,66 @@ import java.lang.StringBuilder;
 import java.io.*;
 
 public class Compressor {
-	public static String compress(String inputUncompressed){
-		StringBuilder result = new StringBuilder(1024);
-		int indexStart = 0;
-		int indexEnd = 0;
-		char compare = 'a';
-		while(indexStart < inputUncompressed.length()){
-			compare = inputUncompressed.charAt(indexStart);
-			while(inputUncompressed.charAt(indexEnd) == compare){
-				indexEnd++;
-				if(indexEnd >= inputUncompressed.length() || indexEnd - indexStart > 14){
+	public static void compressStream(Reader reader, Writer writer) throws IOException {
+		int compare = reader.read();
+		int prch = compare;
+		int count = 0;
+		while(compare > 0){
+
+			while(compare == prch){
+				compare = reader.read();
+				count++;
+				if(count >= 15){
 					break;
 				}
 			}
-			result.append(compare);
-			result.append(Integer.toHexString(indexEnd - indexStart));
-			indexStart = indexEnd;
+			writer.write(prch);
+			writer.write(Integer.toHexString(count));
+			count = 0;
+			prch = compare;
 		}
-		return result.toString();
 	}
-	public static String uncompress(String inputCompressed){
-		StringBuilder result = new StringBuilder(1024);
-		int charindex = 0;
+	public static void uncompressStream(Reader reader, Writer writer) throws IOException {
+		int pchar = 0;
+		int cchar = 0;
 		int count = 0;
-		while(charindex < inputCompressed.length()){
-			count = Character.digit(inputCompressed.charAt(charindex+1),16);
-			for(int i=1; i<=count;i++){
-				result.append(inputCompressed.charAt(charindex));
+		while(pchar >= 0){
+			pchar = reader.read();
+
+			cchar = reader.read();
+			count = Character.isDigit(cchar) ? cchar-0x30 : cchar-0x57;
+			for(int i = 0; i < count; i++){
+				writer.write(pchar);
 			}
-			charindex+=2;
 		}
-		return result.toString();
+	}
+	public static String compressString(String inputUncompressed) throws IOException {
+		StringWriter writer = new StringWriter();
+		compressStream(new StringReader(inputUncompressed), writer);
+		return writer.toString();
+	}
+	public static void compressFile(String inputFileName, String outputFileName) throws IOException {
+		try {
+			FileWriter writer = new FileWriter(new File(outputFileName));
+			compressStream(new FileReader(inputFileName), writer);
+			writer.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		} finally {
+		}
+	}
+	public static String uncompressString(String inputCompressed) throws IOException {
+		StringWriter writer = new StringWriter();
+		uncompressStream(new StringReader(inputCompressed), writer);
+		return writer.toString();
+	}
+	public static void uncompressFile(String inputFileName, String outputFileName) throws IOException{
+		try {
+			FileWriter writer = new FileWriter(new File(outputFileName));
+			uncompressStream(new FileReader(inputFileName), writer);
+			writer.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
 public class Main {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException {
 		String uncompressed = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbccccc";
 		System.out.println(uncompressed);
-		String compressed	= Compressor.compress(uncompressed);
+		String compressed	= Compressor.compressString(uncompressed);
 		System.out.println(compressed);
-		System.out.println(Compressor.uncompress(compressed));
+		System.out.println(Compressor.uncompressString(compressed));
+		Compressor.compressFile("test.txt","test2.txt");
+		Compressor.uncompressFile("test2.txt","test3.txt");
 	}
 }
